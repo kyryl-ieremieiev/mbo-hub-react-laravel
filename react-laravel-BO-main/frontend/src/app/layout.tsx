7f621b47e4b31e77e72066b912d5b0c03fd9d6bc
MsZoezo <zoey@zoezo.dev>
2025-02-25 19:50:01 +0100

    Frontend: Chaos footer

--- react-laravel-BO-main/frontend/src/app/layout.tsx
+++ react-laravel-BO-main/frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
+import Footer from "@/components/footer/footer";
 
 export const metadata: Metadata = {
   title: "MBO Hub",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
