8bb05de156b277c755d3ff88901b7e7e81c98528
Kyrylo <kyrylo@activateyourbusiness.nl>
2025-03-04 09:42:18 +0100

    add home page and limit filters to projects

--- react-laravel-BO-main/frontend/src/util/fetch/useFetch.ts
+++ react-laravel-BO-main/frontend/src/util/fetch/useFetch.ts
@@ -1,13 +1,8 @@
 import getConfig from "next/config";
 import { setCache } from "../cache/useContentCache";
 
-export async function useFetch(url: string, params: Object) { 
-    
-}
-
 export async function fetchContent (type:string, limit?: number | null, params?: Object) {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL
-    console.log(process.env);
     try {
         const response = await fetch(`${apiUrl}/${type}?${params}`, {
             method: 'Get',
