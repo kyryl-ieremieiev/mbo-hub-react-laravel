318c3da832153e252a1bd283e8db059ae827b904
Nickvdt <njm.vandertol@gmail.com>
2025-03-06 10:38:53 +0100

    Nieuws verwijderd

--- react-laravel-BO-main/laravel/routes/api.php
+++ react-laravel-BO-main/laravel/routes/api.php
@@ -6,15 +6,11 @@ use App\Http\Controllers\API\ProjectController;
 use App\Http\Controllers\API\EventController;
 use App\Http\Controllers\API\SkillController;
 use App\Http\Controllers\API\TeamController;
-use App\Http\Controllers\API\NewsController;
 use App\Http\Controllers\API\DeveloperTeamController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-
 
 
 Route::get('/projects', [ProjectController::class, 'index']);
@@ -23,4 +19,3 @@ Route::get('/events', [EventController::class, 'index']);
 Route::get('/skills', [SkillController::class, 'index']);
 Route::get('/teams', [TeamController::class, 'index']);
 Route::get('/developer-teams', [DeveloperTeamController::class, 'index']);
-Route::get('/news', [NewsController::class, 'index']);
