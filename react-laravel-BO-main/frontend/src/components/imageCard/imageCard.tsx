96de14142013f37b30deffac2b9be8908c1c969e
MsZoezo <zoey@zoezo.dev>
2025-03-06 12:41:51 +0100

    Frontend: Project slinger

--- react-laravel-BO-main/frontend/src/components/imageCard/imageCard.tsx
+++ react-laravel-BO-main/frontend/src/components/imageCard/imageCard.tsx
@@ -8,15 +8,21 @@ interface Props {
 
     link?: string;
 
+    banner?: boolean;
+
     children?: ReactNode;
 }
 
-export default function ImageCard({ imagePath, alt = "", children, link = "#" }: Props) {
+export default function ImageCard({ imagePath, alt = "", children, link = "#", banner = false }: Props) {
     return(
         <a href={link} className={styles.card}>
-            <figure className={`${styles.image} ${imagePath ?? styles.empty}`}>
+            <figure className={`${styles.imageWrapper} ${imagePath ?? styles.empty}`}>
                 { imagePath &&
-                    <img src={imagePath} alt={alt} />
+                    <img className={styles.image} src={imagePath} alt={alt} />
+                }
+
+                { banner &&
+                    <img className={styles.banner} src="/images/banner.png" alt="" />
                 }
             </figure>
 
--- react-laravel-BO-main/frontend/src/components/projectCard/projectCard.tsx
+++ react-laravel-BO-main/frontend/src/components/projectCard/projectCard.tsx
@@ -14,7 +14,7 @@ export default function ProjectCard({ project }: Props) {
     const tags = project.tags && project.tags.map((tag: string) => {return {name: tag}});
 
     return (
-        <ImageCard imagePath={project.image ? `${process.env.NEXT_PUBLIC_API_IMG_URL}/${project.image}` : null} link={`/projects/${project.slug}`}>
+        <ImageCard banner={project.amsterdam_750_slider} imagePath={project.image ? `${process.env.NEXT_PUBLIC_API_IMG_URL}/${project.image}` : null} link={`/projects/${project.slug}`}>
             <Tags tags={tags} />
             <h3>{project.title}</h3>
             <div dangerouslySetInnerHTML={{ __html: previewText }} />
